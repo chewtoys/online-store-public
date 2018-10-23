74af8687cb0221535e924d227cf738b5948cdccb
Леонид Ершов <kindle47@outlook.com>
2018-10-23 19:10:58 +0300

    +

--- layouts/default.vue
+++ layouts/default.vue
@@ -23,12 +23,18 @@
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
-    <v-toolbar fixed app :clipped-left="true">
+    <v-toolbar fixed app clipped-left>
       <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
       <img width="32" src="logo.svg">
 
       <v-toolbar-title v-text="title">
       </v-toolbar-title>
+      <v-spacer></v-spacer>
+      
+      <v-btn icon @click="showAuth = true">
+        <v-icon>account_circle</v-icon>
+      </v-btn>
+      <auth-form :show="showAuth" v-on:close="showAuth = false" v-on:save="showAuth = false" />
     </v-toolbar>
     <v-content>
       <v-container>
@@ -57,21 +63,26 @@
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        clipped: false,
-        drawer: true,
-        fixed: false,
-        items: [
-          { icon: 'apps', title: 'Welcome', to: '/' },
-          { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' }
-        ],
-        miniVariant: false,
-        right: true,
-        rightDrawer: false,
-        title: 'Leoneed\'s'
-      }
-    }
+import AuthForm from "~/Components/AuthForm";
+export default {
+  components: {
+    AuthForm
+  },
+  data() {
+    return {
+      showAuth: false,
+      clipped: false,
+      drawer: true,
+      fixed: false,
+      items: [
+        { icon: "apps", title: "Welcome", to: "/" },
+        { icon: "bubble_chart", title: "Inspire", to: "/inspire" }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: "Leoneed's"
+    };
   }
+};
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -67,7 +67,9 @@
 </template>
 
 <script>
+import AuthForm from "~/Components/AuthForm";
 export default {
+  components: { AuthForm },
   data() {
     return {
       lorem: `Lorem ipsum dolor sit amet, mel at clita quando. Te sit oratio vituperatoribus, nam ad ipsum posidonium mediocritatem, explicari dissentiunt cu mea. Repudiare disputationi vim in, mollis iriure nec cu, alienum argumentum ius ad. Pri eu justo aeque torquatos.`
