550fb3f485ea1f961628f5e41ff3c4c8a226b262
Леонид Ершов <kindle47@outlook.com>
2019-01-25 18:32:51 +0300

    lw component wip

---
+++ components/ListView.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-layout row>
+    <v-flex xs12>
+      <v-card max-height="100%">
+        <v-card-title class="blue white--text">
+          <span class="headline">{{Title}}</span>
+          <v-spacer></v-spacer>
+          <!-- <v-menu v-if="isAdmin" bottom left> -->
+          <v-btn v-if="Create">Create</v-btn>
+        </v-card-title>
+        <v-container fluid>
+          <v-layout row wrap justify-space-between>
+            <v-flex xs12 md6 lg4 v-for="item in items" :key="item.ID" class="card-wrap">
+              <slot v-bind:item="item"></slot>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+<script>
+export default {
+  props: {
+    Title: { type: String },
+    Create: { type: Boolean },
+    Update: { type: Boolean },
+    Delete: { type: Boolean },
+    items: [],
+  },
+  data() {
+    return {
+      menuActions: [
+        { title: 'Create', active: false },
+        { title: 'Edit', active: false },
+        { title: 'Delete', active: false },
+      ],
+      categoriesLoaded: false,
+    }
+  },
+  computed: {},
+  methods: {},
+  mounted() {},
+}
+</script>
---
+++ pages/test.vue
@@ -0,0 +1,23 @@
+<template>
+  <list-view :Title="'Test items'" :items="list">
+    <template slot-scope="slotProps">
+      <v-card>
+        <v-card-title>{{ slotProps.item.Title }}</v-card-title>
+      </v-card>
+    </template>
+  </list-view>
+</template>
+<script>
+import ListView from '~/components/ListView.vue'
+
+export default {
+  data() {
+    return {
+      list: [{ ID: 1, Title: 'test obj 1' }, { ID: 2, Title: 'test obj 2' }],
+    }
+  },
+  components: {
+    ListView,
+  },
+}
+</script>
